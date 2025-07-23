980d48fbf536b838166f3b7e04598d3b32de9fb6
LadsForTea <88262542+Absurdnoobman@users.noreply.github.com>
2025-07-23 21:45:03 +0700

    dsdsd

--- MainMenu.vb
+++ MainMenu.vb
@@ -1,5 +1,7 @@
 ﻿Public Class MainMenuForm
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("GGWP")
+        Dim f As New Test
+        f.Show()
     End Sub
 End Class
